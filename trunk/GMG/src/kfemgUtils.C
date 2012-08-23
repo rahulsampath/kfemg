5a207b1156fea9ccbf738e73450052bab6996217
rahul.sampath@gmail.com <rahul.sampath@gmail.com@e090db49-38ab-575a-2ade-22f28abf5d72>
2012-08-23 17:06:45 +0000

    dummy

---
+++ trunk/GMG/src/kfemgUtils.C
@@ -0,0 +1,24 @@
+
+#include "kfemgUtils.h"
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+void read1DshapeFnCoeffs(int K, std::vector<long long int> & coeffs) {
+  char fname[256];
+  sprintf(fname, "C%dShFnCoeffs1D.txt", K);
+
+  FILE *fp = fopen(fname, "r"); 
+
+  int numCoeffs = 4*(K + 1)*(K + 1);
+
+  coeffs.resize(2*numCoeffs);
+  for(int i = 0; i < (2*numCoeffs); ++i) {
+    std::cout << "Read Coeff" << i << std::endl;
+    fscanf(fp, "%lld", &(coeffs[i]));
+  }//end i 
+
+  fclose(fp);
+}
+
+
---
+++ trunk/GMG/src/testRead.C
@@ -0,0 +1,24 @@
+
+#include "kfemgUtils.h"
+#include <cassert>
+#include <iostream>
+
+int main() {
+  std::vector<long long int> coeffs;
+
+  std::cout << "Before Reading "<< std::endl;
+  read1DshapeFnCoeffs(1, coeffs);
+  std::cout << "After Reading "<< std::endl;
+  assert(coeffs.size() == 32);
+  assert(coeffs[0] == 1LL);
+  assert(coeffs[31] == 4LL);
+
+  read1DshapeFnCoeffs(10, coeffs);
+  assert(coeffs.size() == 968);
+  assert(coeffs[0] == 1LL);
+  assert(coeffs[967] == 7431782400LL);
+
+  std::cout<<"Pass!"<<std::endl;
+  return 1;
+}
+
