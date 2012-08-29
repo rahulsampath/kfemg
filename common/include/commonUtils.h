f24825c7863c84b21efa3615d3e462de12fee5ab
rahul.sampath@gmail.com <rahul.sampath@gmail.com@e090db49-38ab-575a-2ade-22f28abf5d72>
2012-08-29 21:00:23 +0000

    dummy

--- common/include/commonUtils.h
+++ common/include/commonUtils.h
@@ -17,15 +17,23 @@ void read1DshapeFnCoeffs(int K, std::vector<long long int> & coeffs);
 void gaussQuad(std::vector<double> & x, std::vector<double> & w);
 
 double eval1DshFn(unsigned int nodeId, unsigned int dofId, unsigned int K, 
-    std::vector<long long int> & coeffs, double x);
+    std::vector<long long int> & coeffs, double xi);
 
 double eval1DshFnDerivative(unsigned int nodeId, unsigned int dofId, unsigned int K,
-    std::vector<long long int> & coeffs, double x, unsigned int l);
+    std::vector<long long int> & coeffs, double xi, unsigned int l);
 
 double powDerivative(double x, unsigned int i, unsigned int l);
 
 inline unsigned long long int factorial(unsigned long long int n) {
   return ( (n <= 1) ? 1 : (n*factorial(n - 1)) );
+}
+
+inline double globalToLocal(double xg, double xa, double hx) {
+  return ( ((xg - xa)*2.0/hx) - 1.0 );
+}
+
+inline double localToGlobal(double xi, double xa, double hx) {
+  return ( xa + (hx*(1 + xi)/2.0) );
 }
 
 #endif
