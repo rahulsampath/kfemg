58ec126e89aa1c9395d053805ceed9957a8fc158
rahul.sampath@gmail.com <rahul.sampath@gmail.com@e090db49-38ab-575a-2ade-22f28abf5d72>
2012-09-05 21:50:17 +0000

    du

--- amg/include/amgUtils.h
+++ amg/include/amgUtils.h
@@ -10,6 +10,8 @@ struct MyMatrix {
   std::vector<std::vector<double> > vals;
 };
 
+void printMatrix(MyMatrix & myMat);
+
 void getNeighbors(std::vector<int> & nh, int zi, int yi, int xi, int Nz, int Ny, int Nx);
 
 void assembleMatrix(MyMatrix & myMat, std::vector<std::vector<double> > const & elemMat, const unsigned int K, 
