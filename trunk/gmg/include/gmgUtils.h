a2ac5643f7bbcf7cb3f449c08488a923101b0d49
rahul.sampath@gmail.com <rahul.sampath@gmail.com@e090db49-38ab-575a-2ade-22f28abf5d72>
2012-09-07 17:43:46 +0000

    du

--- trunk/gmg/include/gmgUtils.h
+++ trunk/gmg/include/gmgUtils.h
@@ -2,5 +2,17 @@
 #ifndef __GMG_UTILS__
 #define __GMG_UTILS__
 
+#include "petsc.h"
+#include "petscvec.h"
+#include "petscmat.h"
+#include "petscda.h"
+#include "petscksp.h"
+#include "petscpc.h"
+#include <vector>
+
+bool foundValidDApart(int dim, PetscInt Nz, PetscInt Ny, PetscInt Nx, int npes);
+
+void createGridSizes(int dim, std::vector<PetscInt> & Nz, std::vector<PetscInt> & Ny, std::vector<PetscInt> & Nx, PetscInt nlevels);
+
 #endif
 
