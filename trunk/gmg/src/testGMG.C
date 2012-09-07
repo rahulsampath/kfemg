84a0db650ff8de44b2cad0a1fe8b24f5626966fb
rahul.sampath@gmail.com <rahul.sampath@gmail.com@e090db49-38ab-575a-2ade-22f28abf5d72>
2012-09-07 14:19:32 +0000

    du

--- trunk/gmg/src/testGMG.C
+++ trunk/gmg/src/testGMG.C
@@ -1,4 +1,8 @@
 
+#include <iostream>
+#include <cassert>
+#include <iomanip>
+#include <cstdlib>
 #include "mpi.h"
 #include "petsc.h"
 #include "common/include/commonUtils.h"
@@ -6,7 +10,50 @@
 
 int main(int argc, char *argv[]) {
   PetscInitialize(&argc, &argv, "options", PETSC_NULL);
-  
+  PetscInt dim = 1; 
+  PetscOptionsGetInt(PETSC_NULL, "-dim", &dim, PETSC_NULL);
+  assert(dim > 0);
+  assert(dim <= 3);
+  std::cout<<"Dim = "<<dim<<std::endl;
+  PetscInt K;
+  PetscOptionsGetInt(PETSC_NULL, "-K", &K, PETSC_NULL);
+  std::cout<<"K = "<<K<<std::endl;
+  PetscTruth useRandomRHS = PETSC_TRUE;
+  PetscOptionsGetTruth(PETSC_NULL, "-useRandomRHS", &useRandomRHS, PETSC_NULL);
+  std::cout<<"Random-RHS = "<<useRandomRHS<<std::endl;
+  PetscInt Nx = 17;
+  PetscOptionsGetInt(PETSC_NULL, "-Nx", &Nx, PETSC_NULL);
+  assert(Nx > 1);
+  std::cout<<"Nx = "<<Nx<<std::endl;
+  PetscInt Ny = 1;
+  if(dim > 1) {
+    PetscOptionsGetInt(PETSC_NULL, "-Ny", &Ny, PETSC_NULL);
+    assert(Ny > 1);
+  }
+  std::cout<<"Ny = "<<Ny<<std::endl;
+  PetscInt Nz = 1;
+  if(dim > 2) {
+    PetscOptionsGetInt(PETSC_NULL, "-Nz", &Nz, PETSC_NULL);
+    assert(Nz > 1);
+  }
+  std::cout<<"Nz = "<<Nz<<std::endl;
+  PetscInt numGrids = 20;
+  PetscOptionsGetInt(PETSC_NULL, "-numGrids", &numGrids, PETSC_NULL);
+  std::cout<<"Max-Num-Grids = "<<numGrids<<std::endl;
+
+  double hx = 1.0/(static_cast<double>(Nx - 1));
+  double hy, hz;
+  if(dim > 1) {
+    hy = 1.0/(static_cast<double>(Ny - 1));
+  }
+  if(dim > 2) {
+    hz = 1.0/(static_cast<double>(Nz - 1));
+  }
+
+  std::vector<long long int> coeffs;
+  read1DshapeFnCoeffs(K, coeffs);
+
+
   PetscFinalize();
 
   return 0;
