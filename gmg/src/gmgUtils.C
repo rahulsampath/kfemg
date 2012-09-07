51c3da056dfb735818ffb4bf23cbda713418be20
rahul.sampath@gmail.com <rahul.sampath@gmail.com@e090db49-38ab-575a-2ade-22f28abf5d72>
2012-09-07 19:17:58 +0000

    du

--- gmg/src/gmgUtils.C
+++ gmg/src/gmgUtils.C
@@ -1,9 +1,65 @@
 
 #include "gmg/include/gmgUtils.h"
-
-void createGridSizes(int dim, std::vector<PetscInt> & Nz, std::vector<PetscInt> & Ny, std::vector<PetscInt> & Nx, PetscInt nlevels) {
-}
+#include <vector>
+#include <cassert>
+#include <iostream>
 
 bool foundValidDApart(int dim, PetscInt Nz, PetscInt Ny, PetscInt Nx, int npes) {
 }
+
+void createGridSizes(int dim, std::vector<PetscInt> & Nz, std::vector<PetscInt> & Ny, std::vector<PetscInt> & Nx) {
+  PetscInt currNx = 17;
+  PetscInt currNy = 1;
+  PetscInt currNz = 1;
+
+  PetscOptionsGetInt(PETSC_NULL, "-finestNx", &currNx, PETSC_NULL);
+  std::cout<<"Nx (Finest) = "<<currNx<<std::endl;
+  if(dim > 1) {
+    PetscOptionsGetInt(PETSC_NULL, "-finestNy", &currNy, PETSC_NULL);
+    std::cout<<"Ny (Finest) = "<<currNy<<std::endl;
+  }
+  if(dim > 2) {
+    PetscOptionsGetInt(PETSC_NULL, "-finestNz", &currNz, PETSC_NULL);
+    std::cout<<"Nz (Finest) = "<<currNz<<std::endl;
+  }
+
+  PetscInt maxNumLevels = 20;
+  PetscOptionsGetInt(PETSC_NULL, "-maxNumLevels", &maxNumLevels, PETSC_NULL);
+  std::cout<<"MaxNumLevels = "<<maxNumLevels<<std::endl;
+
+  Nx.clear();
+  Ny.clear();
+  Nz.clear();
+
+  //0 is the coarsest level.
+  for(int lev = 0; lev < maxNumLevels; ++lev) {
+    Nx.insert(Nx.begin(), currNx);
+    if(dim > 1) {
+      Ny.insert(Ny.begin(), currNy);
+    }
+    if(dim > 2) {
+      Nz.insert(Nz.begin(), currNz);
+    }
+    if( (currNx < 3) || ((currNx%2) == 0) ) {
+      break;
+    }
+    currNx = 1 + ((currNx - 1)/2); 
+    if(dim > 1) {
+      if( (currNy < 3) || ((currNy%2) == 0) ) {
+        break;
+      }
+      currNy = 1 + ((currNy - 1)/2); 
+    }
+    if(dim > 2) {
+      if( (currNz < 3) || ((currNz%2) == 0) ) {
+        break;
+      }
+      currNz = 1 + ((currNz - 1)/2); 
+    }
+  }//lev
+
+  std::cout<<"ActualNumLevels = "<<(Nx.size())<<std::endl;
+}
+
+
 
