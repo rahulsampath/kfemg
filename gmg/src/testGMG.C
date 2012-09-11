adac72fcd7bdf9486920aa74fb198572c14c43da
rahul.sampath@gmail.com <rahul.sampath@gmail.com@e090db49-38ab-575a-2ade-22f28abf5d72>
2012-09-11 14:56:32 +0000

    d

--- gmg/src/testGMG.C
+++ gmg/src/testGMG.C
@@ -22,6 +22,9 @@ int main(int argc, char *argv[]) {
   PetscOptionsGetTruth(PETSC_NULL, "-useRandomRHS", &useRandomRHS, PETSC_NULL);
   std::cout<<"Random-RHS = "<<useRandomRHS<<std::endl;
 
+  int globalRank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &globalRank);
+
   std::vector<long long int> coeffs;
   read1DshapeFnCoeffs(K, coeffs);
 
@@ -35,6 +38,67 @@ int main(int argc, char *argv[]) {
   std::vector<DA> da;
   createDA(da, dofsPerNode, dim, Nz, Ny, Nx, MPI_COMM_WORLD);
 
+  std::vector<Mat> Kmat(da.size(), NULL);
+  for(int i = 0; i < da.size(); ++i) {
+    if(da[i] != NULL) {
+      DAGetMatrix(da[i], MATAIJ, &(Kmat[i]));
+    }
+  }//end i
+
+  std::vector<Mat> Pmat((da.size() - 1), NULL);
+
+  KSP ksp;
+  Vec rhs;
+  Vec sol;
+
+  assert(da[da.size() - 1] != NULL);
+  DAGetGlobalVector(da[da.size() - 1], &rhs);
+  DAGetGlobalVector(da[da.size() - 1], &sol);
+
+  const unsigned int seed = (0x3456782  + (54763*globalRank));
+  PetscRandom rndCtx;
+  PetscRandomCreate(MPI_COMM_WORLD, &rndCtx);
+  PetscRandomSetType(rndCtx, PETSCRAND48);
+  PetscRandomSetSeed(rndCtx, seed);
+  PetscRandomSeed(rndCtx);
+  VecSetRandom(sol, rndCtx);
+  //ZERO BOUNDARIES ????
+  PetscRandomDestroy(rndCtx);
+  assert(Kmat[Kmat.size() - 1] != NULL);
+  MatMult(Kmat[Kmat.size() - 1], sol, rhs);
+
+  VecZeroEntries(sol);
+
+  KSPSolve(ksp, rhs, sol);
+
+  KSPDestroy(ksp);
+
+  DARestoreGlobalVector(da[da.size() - 1], &rhs);
+  DARestoreGlobalVector(da[da.size() - 1], &sol);
+
+  for(int i = 0; i < da.size(); ++i) {
+    if(da[i] != NULL) {
+      DADestroy(da[i]);
+    }
+    da[i] = NULL;
+  }//end i
+  da.clear();
+
+  for(int i = 0; i < Kmat.size(); ++i) {
+    if(Kmat[i] != NULL) {
+      MatDestroy(Kmat[i]);
+    }
+    Kmat[i] = NULL;
+  }//end i
+  Kmat.clear();
+
+  for(int i = 0; i < Pmat.size(); ++i) {
+    if(Pmat[i] != NULL) {
+      MatDestroy(Pmat[i]);
+    }
+    Pmat[i] = NULL;
+  }//end i
+  Pmat.clear();
 
   PetscFinalize();
 
